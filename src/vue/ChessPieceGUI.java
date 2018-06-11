9c12258e6f85dd465d1ce8ef7630c31897bf5e8b
MathisDELOGE <thisdemalo@gmail.com>
2018-06-11 15:35:10 +0200

    Les pions s'affichent

--- src/vue/ChessPieceGUI.java
+++ src/vue/ChessPieceGUI.java
@@ -4,8 +4,8 @@ import tools.data.Couleur;
 
 import javax.swing.*;
 
-public class ChessPieceGUI extends JPanel {
+public class ChessPieceGUI extends JLabel {
     public ChessPieceGUI(Couleur couleur, ImageIcon imageIcon) {
-
+        super(imageIcon);
     }
 }
--- src/vue/ChessSquareGUI.java
+++ src/vue/ChessSquareGUI.java
@@ -1,10 +1,18 @@
 package vue;
 
+import tools.data.Coord;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class ChessSquareGUI extends JPanel {
-    public ChessSquareGUI(int i, int j, Color white) {
+    private Coord coords;
+    private Color color;
 
+    public ChessSquareGUI(int x, int y, Color color) {
+        coords = new Coord(x,y);
+        this.color = color;
+
+        this.setBackground(this.color);
     }
 }
