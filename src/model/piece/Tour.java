9dcf711798f549d5de9461a5345fd023af6435fd
MathisDELOGE <thisdemalo@gmail.com>
2018-06-13 14:09:32 +0200

    Implémentation des règles de déplacement des pieces

--- src/model/piece/Tour.java
+++ src/model/piece/Tour.java
@@ -1,5 +1,6 @@
 package model.piece;
 
+import tools.BoardGameConfig;
 import tools.data.Coord;
 import tools.data.Couleur;
 
@@ -12,7 +13,11 @@ public class Tour extends AbstractPiece {
 
     @Override
     public boolean isAlgoMoveOk(int xFinal, int yFinal) {
-        return false;
+        boolean res = false;
+        if ((xFinal == this.getX() && yFinal != this.getY()) || (yFinal == this.getX() && xFinal != this.getX()));
+            if (xFinal >= 0 && xFinal <= BoardGameConfig.getNbColonne() && yFinal >= 0 && yFinal <= BoardGameConfig.getNbLigne())
+                res = true;
+        return res;
     }
 
     @Override
